7b84e208c4e29cc2e7cb890dff849549be49350c
Leonardo Vieira <leonardovieira1993@gmail.com>
2025-02-19 21:07:46 -0300

    Commit inicial da base do projeto

---
+++ Sistema-gerenciamento/src/main/java/com/concessionaria/Sistema_gerenciamento/SistemaGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package com.concessionaria.Sistema_gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoApplication.class, args);
+	}
+
+}
---
+++ Sistema-gerenciamento/src/test/java/com/concessionaria/Sistema_gerenciamento/SistemaGerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concessionaria.Sistema_gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
